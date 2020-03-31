4eb21d725106edfb5116b2d3afadc0b5349852e5
solanav <solanav@qq.com>
2020-03-31 20:32:48 +0200

    IDX reading and number drawing

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,54 @@
-use ndarray::{Array1, Array2, Array};
-use rand::Rng;
-use rand::prelude::ThreadRng;
+use ndarray::{Array1, Array2};
 use ndarray_rand::RandomExt;
 use rand::distributions::Uniform;
+use rayon::prelude::*;
+use idx_decoder::IDXDecoder;
+use std::f64::EPSILON;
 
 const INPUT_SIZE: usize = 784;
 const HIDDEN_SIZE: usize = 16;
 const OUTPUT_SIZE: usize = 10;
 
 #[inline]
-fn relu(x: f64) -> f64 {
-    match x {
-        x if x < 0f64 => 0f64,
-        x => x,
-    }
-}
+fn sigmoid(x: f64) -> f64 { 1f64 / (1f64 + (EPSILON.powf(-x))) }
+
+#[inline]
+fn clamp(x: u8) -> f64 { x as f64 / 255f64 }
 
 // Given a layer, weights and biases it returns the value of the next layer
 fn transition(l: Array1<f64>, w: Array2<f64>, b: Array1<f64>) -> Array1<f64> {
     let a1: Array1<f64> = w.dot(&l) + b;
-    a1.map(|&e| relu(e))
+    a1.map(|&e| sigmoid(e))
+}
+
+fn draw_vec(v: Vec<u8>) {
+    let mut c = -1;
+    for x in v {
+        c += 1;
+
+        let printable_x = match x {
+            x if x > 170 => "\u{2593}\u{2593}",
+            x if x > 85 => "\u{2592}\u{2592}",
+            x if x > 0 => "\u{2591}\u{2591}",
+            _ => " ",
+        };
+
+        print!("{} ", printable_x);
+
+        if c == 27 {
+            c = -1;
+            println!();
+        }
+    }
 }
 
 fn main() {
-    let dist = Uniform::new(0f64, 1f64);
+    let f = std::fs::File::open("./train-images.idx3-ubyte").unwrap();
+    let mut data = IDXDecoder::<_, idx_decoder::types::U8, nalgebra::U3>::new(f).unwrap();
+    let f = std::fs::File::open("./train-labels.idx1-ubyte").unwrap();
+    let mut labels = IDXDecoder::<_, idx_decoder::types::U8, nalgebra::U1>::new(f).unwrap();
 
-    // Layers with a random input for now
-    let l0 = Array1::<f64>::random(INPUT_SIZE, dist);
-    let l1;
-    let l2;
-    let l3;
+    let dist = Uniform::new(0f64, 1f64);
 
     // Random weights (connections between layers)
     let w0 = Array2::<f64>::random((HIDDEN_SIZE, INPUT_SIZE), dist);
@@ -41,11 +60,15 @@ fn main() {
     let b1 = Array1::<f64>::random(HIDDEN_SIZE, dist);
     let b2 = Array1::<f64>::random(OUTPUT_SIZE, dist);
 
-    l1 = transition(l0.clone(), w0, b0);
-    l2 = transition(l1.clone(), w1, b1);
-    l3 = transition(l2.clone(), w2, b2);
+    let image: Vec<f64> = data.next().unwrap()
+        .iter()
+        .map(|&e| sigmoid(e as f64))
+        .collect();
 
-    println!("{}", l0);
-    println!("{}", l1);
-    println!("{}", l2);
+    let answer = data.next().unwrap();
+
+    let l0 = Array1::<f64>::from_shape_vec(INPUT_SIZE, image).unwrap();
+    let l1 = transition(l0.clone(), w0.clone(), b0.clone());
+    let l2 = transition(l1.clone(), w1.clone(), b1.clone());
+    let l3 = transition(l2.clone(), w2.clone(), b2.clone());
 }
